5f7440d17b326b3f8c8b0545c413696643e951c1
Nerielle <nelya.iva@gmail.com>
2015-06-16 17:32:28 +0300

    test connection to github

---
+++ Dijkstra/Edge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dijkstra
+{
+    public struct Edge : IComparable<Edge>
+    {
+        public Edge(int labelFrom, int labelTo, int length)
+            : this()
+        {
+            LabelTo = labelTo;
+            Length = length;
+            LabelFrom = labelFrom;
+        }
+
+        public int LabelFrom { get; set; }
+        public int LabelTo { get; set; }
+        public int Length { get; set; }
+
+        public int CompareTo(Edge other)
+        {
+            if (Length < other.Length)
+                return -1;
+            return Length == other.Length ? 0 : 1;
+        }
+    }
+}
---
+++ Dijkstra/VertexInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dijkstra
+{
+    public class VertexInfo
+    {
+        public VertexInfo(int label, Edge[] edges)
+        {
+            Label = label;
+            Edges = edges ?? new Edge[0];
+            EdgesWithoutLoops = ExcludeLoops();
+        }
+
+        private Edge[] Edges { get; set; }
+        public List<Edge> EdgesWithoutLoops { get; set; }
+        public int Label { get; set; }
+
+        public void ExcludeFoundedPathsFromEdges(int labelTo)
+        {
+            EdgesWithoutLoops.RemoveAll(x => x.LabelTo == labelTo);
+        }
+
+        public Edge GetMinEdge()
+        {
+            return EdgesWithoutLoops.Min();
+        }
+
+        private List<Edge> ExcludeLoops()
+        {
+            return Edges.Where(x => x.LabelTo != Label).ToList();
+        }
+    }
+}
