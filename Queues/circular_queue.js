b4272764ab119899d4187a6436a20aa3477b5b8c
Nerielle <nelya.iva@gmail.com>
2020-10-07 16:09:48 +0100

    naive realization

--- Queues/circular_queue.js
+++ Queues/circular_queue.js
@@ -6,10 +6,16 @@ class CircularQueue {
     this.write = 0;
     this.max = size - 1;
 
+
     while (size > 0) {
       this.queue.push(null);
       size--;
     }
+  }
+  
+  isFull(){
+      return (this.write === this.queue.length -1 && this.read === 0)
+        || (write === read -1);
   }
 
   print() {
@@ -17,10 +23,22 @@ class CircularQueue {
   }
 
   enqueue(item) {
-
+      var insertIndex = this.write;
+      this.queue[insertIndex] = item;
   }
 
   dequeue() {
-    
+    var popIndex = this.read;
+    var item = this.queue[popIndex];
+    this.queue[popIndex] = null;
+    return item;
   }
 }
+
+var q = new CircularQueue(5);
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueue(4);
+q.enqueue(5);
+q.enqueue(6);
