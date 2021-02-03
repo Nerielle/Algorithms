c41065273b7c1613c1281d6a6ff101746118efd7
Nerielle <nelya.iva@gmail.com>
2021-02-03 10:52:59 +0000

    init surrounded regions

---
+++ LeetCode/SurroundedRegions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SurroundedRegions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Solve(char[][] board)
+        {
+
+        }
+    }
+}
