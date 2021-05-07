cfdccce7af90f3999bfeee63b10f92ce860ecfdd
Nailia Abzalilova <nelya.iva@gmail.com>
2021-05-07 15:26:34 +0100

    second largest number in the array (initial)

---
+++ LeetCode/DivideAndConquer/SecondLargest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SecondLargest
+{
+    class Program
+    {
+        int[] array = new int[]{9,8,7,4};
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
