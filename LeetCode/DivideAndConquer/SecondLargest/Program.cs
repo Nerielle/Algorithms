4399c46a2fd483df489a7f76bc2293613a563c8f
Nailia Abzalilova <nelya.iva@gmail.com>
2021-05-26 18:55:50 +0100

    impelementation

--- LeetCode/DivideAndConquer/SecondLargest/Program.cs
+++ LeetCode/DivideAndConquer/SecondLargest/Program.cs
@@ -1,13 +1,73 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SecondLargest
 {
     class Program
     {
-        int[] array = new int[]{9,8,7,4};
+        static int[] array = new int[]{10,9,5,4,11,100,120,110};// { 7, 3, 11, 9, 2, 1, 12, 14, 6, 10 };
         static void Main(string[] args)
         {
+            var tmp = new Dictionary<int, List<int>>();
+            var res = FindMax(array, tmp);
+
+            Console.WriteLine(res);
+        }
+
+        private static int? FindMax(int[] array, Dictionary<int, List<int>> tmp)
+        {
+            
+            var maxIndex = ComparePairs(array, array.Select((x, i)=> i).ToList(), 0, tmp);
+             var indexesToCompare = new List<int>();
+             var index = maxIndex;
+            for(var j=0;j<tmp.Keys.Count;j++)
+            {
+                var levelIds = tmp[j];
+               indexesToCompare.Add(levelIds[index/2]);
+               index = index/2;
+           }
+           var second = ComparePairs(array, indexesToCompare, 0, new Dictionary<int, List<int>>());
+            return array[second];
+        }
+
+        private static int ComparePairs(int[] array, List<int> levelIndexes, int level, Dictionary<int, List<int>> minlevelIndexes)
+        {
+            if(levelIndexes.Count == 1){
+                return levelIndexes.First();
+            }
+            var newLevel = new List<int>();
+            minlevelIndexes.Add(level, new List<int>());
+            for (int i = 0; i < levelIndexes.Count-1; i = i + 2)
+            {
+                var first = array[levelIndexes[i]];
+                var second = array[levelIndexes[i+1]];
+                if (first < second)
+                {
+                    minlevelIndexes[level].Add(levelIndexes[i]);
+                    newLevel.Add(levelIndexes[i+1]);
+                }
+                else
+                {
+                     minlevelIndexes[level].Add(levelIndexes[i+1]);
+                     newLevel.Add(levelIndexes[i]);
+                }
+                
+            }
+            if(levelIndexes.Count %2==1){
+                newLevel.Add(levelIndexes.Last());
+            }
+            
+            return ComparePairs(array, newLevel, level +1, minlevelIndexes);
             
         }
+
     }
+
+
+
+
+
+
 }
--- LeetCode/DivideAndConquer/SecondLargest/readme.md
+++ LeetCode/DivideAndConquer/SecondLargest/readme.md
@@ -1,3 +1,13 @@
-You are given as input an unsorted <b>array</b> of n distinct numbers, where <b>n</b> is a power of <b>2</b>.
+<h2>Problem</h2>
+You are given as input an unsorted <b>array</b> of <em>n</em> distinct numbers, where <b>n</b> is a power of <b>2</b>.
 
 Give an algorithm that identifies the <b>second-largest number</b> in the array, and that uses at most <em>n+log<sub>2</sub> n−2</em> comparisons.
+
+<h2>Implementation</h2> 
+We compare elements on odd and even positions and configure a new array consisting of maximum of each pair. We do that recursively untill we find max element. 
+It would be <em>n-1</em> comparisons.
+We have compaired max element of the array with  <em>log<sub>2</sub> n</em> another elements.
+
+So the entire number of comparisons would be:
+<em>n+log<sub>2</sub> n -1</em>.
+
